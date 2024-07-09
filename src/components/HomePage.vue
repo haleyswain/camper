8a32c1cdbd3fc32daf7cc93accfc200dfdb56aa3
Haley Philips <hswain@rei.com>
2024-07-08 20:29:58 -0700

    clean up unneeded scaffolding

---
+++ src/components/HomePage.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <h1>Camper</h1>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+</style>
